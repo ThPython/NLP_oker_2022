22443ef3e3234389991b5d3e92dad3b674b8674b
Burghardt Thomas <50694751+ThPython@users.noreply.github.com>
2022-08-28 23:16:42 +0200

    Update politSplitToYear.py

--- scripts/politSplitToYear.py
+++ scripts/politSplitToYear.py
@@ -1,21 +1,9 @@
-### politSplitToYear.py
-
-
-Ad 1.) politSplitToYear.py
-nimmt die Datei 'only_political_articles_with_scores.csv' als Eingabe und erzeugt daraus 2 Dateien:
-a) df_2016_descend.csv    # Artikel für das Jahr 2016, absteigend nach 'compound' sortiert (1 bis -1)
-b) df_2017_descend.csv    # Artikel für das Jahr 2017, absteigend nach 'compound' sortiert (1 bis -1)
-
-Ad 2.) polit_sent.py
-nimmt die von politSplitToYear.py erzeugten 2 Dateien als Eingabe und gibt verdichtete Daten aus:
-.
-.
-.
-c758046b36cbea162f559fff1ab39f18 2017-02-24T20:17:56Z -0.9998 2017
-7d2a0432-ea4a-11e6-bf6f-301b6b443624 2017-02-04T00:28:20Z -0.9998 2017
-2016: von 1908 Artikeln waren 1392 (72%) positiv und 516 (28%) negativ
-2017: von 1368 Artikeln waren 999 (73%) positiv und 369 (27%) negativ
-###
+# politSplitToYear.py nimmt die Datei 'only_political_articles_with_scores.csv' als Eingabe und erzeugt daraus 2 Dateien:
+# a) df_2016_descend.csv    # Artikel für das Jahr 2016, absteigend nach 'compound' sortiert (1 bis -1)
+# b) df_2017_descend.csv    # Artikel für das Jahr 2017, absteigend nach 'compound' sortiert (1 bis -1)
+# 2016: von 1908 Artikeln waren 1392 (72%) positiv und 516 (28%) negativ
+# 2017: von 1368 Artikeln waren 999 (73%) positiv und 369 (27%) negativ
+# Alle benötigten Dateien liegen auf Sciebo, aufgrund der bereits unterschriebenen "Individual Application".
 
 import pandas as pd
 
